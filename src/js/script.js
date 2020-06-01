e67eb695d0abc48a3981d3f5f7bd0b381169435c
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-05-31 23:17:53 -0300

    Add script.js file

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+let filterInput = document.getElementById('filterInput');
+
+//Add Event Listener
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames() {
+    let filterValue = filterInput.value.toUpperCase();  
+    
+    //Get names ul
+    let ul = document.getElementById('names');
+
+    //Get lis from ul
+    let li = ul.querySelectorAll('li.collection-item');
+
+    //Loop through collection of lis
+    for(let i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];      
+
+        //if matched
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+           li[i].style.display = '';        
+        }else {
+            li[i].style.display = 'none';
+        }
+    }
+}
+
+
